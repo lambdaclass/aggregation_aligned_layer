20a516e080e091128e08740788c69b56a0978dfd
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-03 13:01:28 -0300

    Serialize more data

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,9 +1,11 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
+use lambdaworks_crypto::commitments::kzg::StructuredReferenceString;
 use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::curve::BLS12381Curve;
 use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::default_types::FrElement;
 use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::default_types::FrField;
+use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::twist::BLS12381TwistCurve;
 use lambdaworks_math::elliptic_curve::short_weierstrass::point::ShortWeierstrassProjectivePoint;
 use lambdaworks_math::traits::Deserializable;
 use lambdaworks_plonk::prover::Proof;
@@ -14,8 +16,12 @@ use lambdaworks_plonk::test_utils::utils::KZG;
 use lambdaworks_plonk::verifier::Verifier;
 
 fn main() {
-    let proof_path = sp1_zkvm::io::read::<String>();
-    let vk_path = sp1_zkvm::io::read::<String>();
+    let mut proof_bytes = [0u8; 1620];
+    sp1_zkvm::io::read_slice(&mut proof_bytes);
+    let mut srs_bytes = [0u8; 1596];
+    sp1_zkvm::io::read_slice(&mut srs_bytes);
+    let mut vk_bytes = [0u8; 1152];
+    sp1_zkvm::io::read_slice(&mut vk_bytes);
 
     // Circuit for the program:
     //  public input x
@@ -24,19 +30,22 @@ fn main() {
     //  z = x * e
     //  assert y == z
     let circuit = test_common_preprocessed_input_1();
+    // let srs = test_srs(circuit.n);
 
-    let srs = test_srs(circuit.n);
+    let srs: StructuredReferenceString<
+        ShortWeierstrassProjectivePoint<BLS12381Curve>,
+        ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+    > = StructuredReferenceString::deserialize(&srs_bytes).expect("Could not deserialize SRS");
+
     let kzg = KZG::new(srs);
 
     let x: FrElement = FrElement::from(4_u64);
     let y: FrElement = FrElement::from(12_u64);
     let pub_input = vec![x, y];
 
-    let proof_bytes = std::fs::read(proof_path).expect("Could not read proof file");
     let proof: Proof<FrField, KZG> =
         Proof::deserialize(&proof_bytes).expect("Could not deserialize PLONK proof");
 
-    let vk_bytes = std::fs::read(vk_path).expect("Could not read verification key file");
     let vk: VerificationKey<ShortWeierstrassProjectivePoint<BLS12381Curve>> =
         VerificationKey::deserialize(&vk_bytes)
             .expect("Could not deserialize PLONK verification key");
