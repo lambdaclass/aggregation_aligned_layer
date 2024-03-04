ccef1bee34c1576e36cc8c63cb4fad4f6f66b681
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-04 11:30:53 -0300

    Add pairing verifier program and script

---
+++ pairing_verifier/src/main.rs
@@ -0,0 +1,35 @@
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use lambdaworks_math::cyclic_group::IsGroup;
+use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::curve::BLS12381Curve;
+use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::pairing::BLS12381AtePairing;
+use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::twist::BLS12381TwistCurve;
+use lambdaworks_math::elliptic_curve::traits::IsEllipticCurve;
+use lambdaworks_math::elliptic_curve::traits::IsPairing;
+use lambdaworks_math::field::element::FieldElement;
+use lambdaworks_math::unsigned_integer::element::U384;
+
+fn main() {
+    let p = BLS12381Curve::generator();
+    let q = BLS12381TwistCurve::generator();
+    let a = U384::from_u64(11);
+    let b = U384::from_u64(93);
+
+    let result = BLS12381AtePairing::compute_batch(&[
+        (
+            &p.operate_with_self(a).to_affine(),
+            &q.operate_with_self(b).to_affine(),
+        ),
+        (
+            &p.operate_with_self(a * b).to_affine(),
+            &q.neg().to_affine(),
+        ),
+    ])
+    .unwrap();
+
+    let verification_result = result == FieldElement::one();
+
+    // We write the result of the verification into the output.
+    sp1_zkvm::io::write::<bool>(&verification_result);
+}
