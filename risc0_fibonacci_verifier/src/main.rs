4d2a208292188c8c2abe3f13ead0f9f7ff755a84
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2024-03-05 18:43:58 -0300

    Add risc0 folders

---
+++ risc0_fibonacci_verifier/src/main.rs
@@ -0,0 +1,18 @@
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use risc0_zkvm::Receipt;
+
+pub const FIBONACCI_GUEST_ID: [u32; 8] = [
+    3415224240, 3079988862, 1684040742, 2802764917, 936974713, 2148640136, 196309035, 667738874,
+];
+
+fn main() {
+    let mut receipt_bytes = [0u8; 215514];
+    // sp1_zkvm::io::read_slice(&mut receipt_bytes);
+
+    let receipt: Receipt = bincode::deserialize(&receipt_bytes).unwrap();
+    let verification_result = receipt.verify(FIBONACCI_GUEST_ID).is_ok();
+
+    // sp1_zkvm::io::write::<bool>(&verification_result);
+}
---
+++ risc0_fibonacci_verifier_script/src/main.rs
@@ -0,0 +1,32 @@
+//! A simple script to generate and verify the proof of a given program.
+use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+
+const ELF: &[u8] =
+    include_bytes!("../../ark_bn254_pairing_verifier/elf/riscv32im-succinct-zkvm-elf");
+const RISC0_FIB_RECEIPT: &[u8] =
+    include_bytes!("../../risc0_fibonacci_verifier/proving_data/risc0_fibo.proof");
+
+fn main() {
+    sp1_core::utils::setup_logger();
+
+    let mut stdin = SP1Stdin::new();
+    stdin.write_slice(RISC0_FIB_RECEIPT);
+
+    println!("Starting sp1 proof generation...");
+    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+
+    let receipt_verification_result = proof.stdout.read::<bool>();
+    println!(
+        "RISC0 fibonacci receipt verification result: {}",
+        receipt_verification_result
+    );
+
+    // Verify proof.
+    SP1Verifier::verify(ELF, &proof).expect("SP1 verification failed");
+    println!("SP1 proof verified successfully");
+
+    // Save proof.
+    let sp1_proof_path = "risc0-fib-proof-with-io.json";
+    proof.save(sp1_proof_path).expect("saving proof failed");
+    println!("Proof saved to {}", sp1_proof_path);
+}
